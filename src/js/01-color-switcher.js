0d8b0b2d1f91fd52cc2ca6cf192348533cb13145
Oleksandr Kurzhy <103747805+voanergesKM@users.noreply.github.com>
2022-08-11 15:15:23 +0300

    Task #1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+let randomHexColor = null;
+let intervalid = null;
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+refs.stopBtn.setAttribute('disabled', '');
+
+function onStartBtnClick() {
+  refs.stopBtn.removeAttribute('disabled');
+  refs.startBtn.setAttribute('disabled', '');
+
+  intervalid = setInterval(() => changeBodyBgColor(), 1000);
+}
+
+function onStopBtnClick() {
+  refs.startBtn.removeAttribute('disabled');
+  refs.stopBtn.setAttribute('disabled', '');
+
+  clearInterval(intervalid);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBodyBgColor() {
+  randomHexColor = getRandomHexColor();
+
+  refs.body.style.backgroundColor = randomHexColor;
+}
