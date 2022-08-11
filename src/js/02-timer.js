9402570c435f97b3339fb97e17ec21282846af9b
Oleksandr Kurzhy <103747805+voanergesKM@users.noreply.github.com>
2022-08-11 18:03:47 +0300

    Task #2 added notiflix lib

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,6 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refs = {
   inputEl: document.querySelector('[datetime-picker]'),
@@ -26,11 +27,19 @@ const options = {
   onClose(selectedDates) {
     dateInFuture = selectedDates[0];
     if (currentDate > selectedDates[0]) {
-      alert('Please choose a date in the future');
+      Notify.failure('Please choose a date in the future', {
+        position: 'center-top',
+        cssAnimationStyle: 'zoom',
+      });
+
       refs.startBtn.setAttribute('disabled', '');
       return;
     } else {
       refs.startBtn.removeAttribute('disabled');
+
+      Notify.success('Back to the future!', {
+        position: 'center-top',
+      });
     }
   },
 };
